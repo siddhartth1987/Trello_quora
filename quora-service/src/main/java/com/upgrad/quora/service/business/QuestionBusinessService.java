540a3e6540fe55beb0d18cca49a868da64e3fc9b
siddhartth1987 <siddhartth.1987@gmail.com>
2021-02-20 15:37:50 +0530

    Made a small change to method signature that is calling UserDAO to get user details using userID

--- quora-service/src/main/java/com/upgrad/quora/service/business/QuestionBusinessService.java
+++ quora-service/src/main/java/com/upgrad/quora/service/business/QuestionBusinessService.java
@@ -7,14 +7,15 @@ import com.upgrad.quora.service.entity.UserEntity;
 import com.upgrad.quora.service.exception.AuthorizationFailedException;
 import com.upgrad.quora.service.exception.InvalidQuestionException;
 import com.upgrad.quora.service.exception.UserNotFoundException;
-import java.time.ZonedDateTime;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.UUID;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.time.ZonedDateTime;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
 
 @Service
 public class QuestionBusinessService {
@@ -100,7 +101,7 @@ public class QuestionBusinessService {
         UserEntity userEntity = authenticationService.validateAuthToken(authorization);
         List<QuestionEntity> questionEntity = new ArrayList<QuestionEntity>();
         if (userEntity != null) {
-            UserEntity questionUser = userDao.getUserById(userId);
+            UserEntity questionUser = userDao.getUser(userId);
             if (questionUser == null) {
                 throw new UserNotFoundException("USR-001", "User with entered uuid whose question details are to be seen does not exist");
             }
